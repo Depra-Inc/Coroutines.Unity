19fe8dbf90a6f1afca664885994889aeaeda1421
godzzz <g0dzZz1lla@yandex.ru>
2023-03-07 15:13:15 +0400

    Added editor coroutine & host

---
+++ Editor/EditorCoroutineHost.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using Depra.Coroutines.Runtime;
+
+namespace Depra.Coroutines.Editor
+{
+    public sealed class EditorCoroutineHost : ICoroutineHost
+    {
+        public ICoroutine StartCoroutine(IEnumerator enumerator) =>
+            new EditorCoroutineProxy(enumerator);
+
+        public void StopCoroutine(ICoroutine coroutine) =>
+            coroutine.Stop();
+    }
+}
---
+++ Editor/EditorCoroutineProxy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using Depra.Coroutines.Runtime;
+using Unity.EditorCoroutines.Editor;
+
+namespace Depra.Coroutines.Editor
+{
+    public sealed class EditorCoroutineProxy : ICoroutine
+    {
+        private EditorCoroutine _coroutine;
+
+        public EditorCoroutineProxy(IEnumerator enumerator) =>
+            Enumerator = enumerator;
+
+        public IEnumerator Enumerator { get; }
+
+        public bool Done => _coroutine == null;
+
+        public void Start()
+        {
+            _coroutine = EditorCoroutineUtility.StartCoroutine(Enumerator, this);
+        }
+
+        public void Stop()
+        {
+            if (Done)
+            {
+                return;
+            }
+
+            EditorCoroutineUtility.StopCoroutine(_coroutine);
+            _coroutine = null;
+        }
+    }
+}
